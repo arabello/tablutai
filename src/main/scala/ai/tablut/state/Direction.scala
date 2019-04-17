90dadd1af89cd206241a8bf1f9abf2d5fde96941
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-04-17 21:08:11 +0200

    Add Direction and Way

---
+++ src/main/scala/ai/tablut/state/Direction.scala
@@ -0,0 +1,6 @@
+package ai.tablut.state
+
+object Direction extends Enumeration {
+	type Direction = Value
+	val HORIZONTAL, VERTICAL = Value
+}
---
+++ src/main/scala/ai/tablut/state/Way.scala
@@ -0,0 +1,6 @@
+package ai.tablut.state
+
+object Way extends Enumeration {
+	type Way = Value
+	val UP, RIGHT, DOWN, LEFT = Value
+}
