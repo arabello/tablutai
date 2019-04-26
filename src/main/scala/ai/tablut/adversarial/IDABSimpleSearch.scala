18b945a374cf73ac36f0f3a8d30a9c14bc0f3e85
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-04-26 21:02:42 +0200

    Add Scala IDAB search implementation

--- src/main/scala/ai/tablut/adversarial/IDABSimpleSearch.scala
+++ src/main/scala/ai/tablut/adversarial/IDABSimpleSearch.scala
@@ -6,11 +6,10 @@ import ai.tablut.adversarial.heuristic.{HeuristicBuilder, HeuristicFunction, Nor
 import ai.tablut.state.Player.Player
 import ai.tablut.state.implicits._
 import ai.tablut.state.{Player, _}
-import aima.core.search.adversarial.IterativeDeepeningAlphaBetaSearch
 
 import scala.collection.JavaConverters._
 
-class IDABSimpleSearch(context: GameContext, game: TablutGame, time: Int) extends IterativeDeepeningAlphaBetaSearch(game, 0, 1, time){
+class IDABSimpleSearch(context: GameContext, game: TablutGame, time: Int) extends IDABSearch(game, 0, 1, time){
 
 	private val hKingAssasination: HeuristicFunction = NormalGameHeuristicFactory.createKingAssasination()
 	private val hBlockEscapePoints: HeuristicFunction = NormalGameHeuristicFactory.createBlockEscapePoints()
@@ -39,15 +38,15 @@ class IDABSimpleSearch(context: GameContext, game: TablutGame, time: Int) extend
 		hValue
 	}
 
-	override def orderActions(state: State, actions: util.List[Action], player: Player.Value, depth: Int): util.List[Action] = player match {
-		case Player.WHITE => actions.asScala.sortWith((a1, a2) => a2.who == CellContent.KING).asJava
+	override def orderActions(state: State, actions: util.List[Action], player: Player.Value, depth: Int): List[Action] = player match {
+		case Player.WHITE => actions.asScala.sortWith((a1, a2) => a2.who == CellContent.KING).toList
 		case Player.BLACK =>
 			val king = state.findKing
 			if (king.isEmpty)
-				return actions
+				return actions.asScala.toList
 
 			val kingSurrounding = king.get.surroundingAt(1)(state).filter(c => c.isDefined && c.get.cellContent == CellContent.EMPTY).map(c => c.get)
-			actions.asScala.sortWith((a1, a2) => kingSurrounding.contains(a2.to)).asJava
+			actions.asScala.sortWith((a1, a2) => kingSurrounding.contains(a2.to)).toList
 	}
 
 }
