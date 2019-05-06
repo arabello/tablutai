338921fff168ad049bdb9a02ae15c9ea21b5ec30
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-05-06 20:53:49 +0200

    Differentiate Heuristics over time

--- src/main/scala/ai/tablut/adversarial/heuristic/HeuristicFactory.scala
+++ src/main/scala/ai/tablut/adversarial/heuristic/HeuristicFactory.scala
@@ -5,24 +5,43 @@ import ai.tablut.state.{GameContext, Player}
 import ai.tablut.state.Player.Player
 
 object HeuristicFactory{
-	private def whiteStrategies(gameContext: GameContext) = Seq(
-		(new CellPriorityStrategy(gameContext), 6),
-		//(new KingEscapeStrategy(gameContext), 3),
-		(new PawsMajorityStrategy(), 3),
-		(new StarterStrategy(gameContext), 10)
-	)
+	private def whiteStrategies(gameContext: GameContext, phase: Phase) = phase match{
+		case Phase.START => Seq(
+			(new StarterStrategy(gameContext), 8),
+			(new PawsMajorityStrategy(), 2)
+		)
+		case Phase.MID => Seq(
+			(new CellPriorityStrategy(gameContext), 6),
+			(new KingEscapeStrategy(gameContext), 3),
+			(new PawsMajorityStrategy(), 1),
+		)
+		case Phase.END => Seq(
+			(new KingEscapeStrategy(gameContext), 8),
+			(new PawsMajorityStrategy(), 2),
+		)
+	}
 
-	private def blackStrategies(gameContext: GameContext) = Seq(
-		(new KingKillingStrategy, 6),
-		(new PawsMajorityStrategy(), 2),
-		(new KingEscapeStrategy(gameContext), 1),
-		(new HotspotStrategy(gameContext), 1)
-	)
+	private def blackStrategies(gameContext: GameContext, phase: Phase) = phase match{
+		case Phase.START => Seq(
+			(new StarterStrategy(gameContext), 8),
+			(new PawsMajorityStrategy(), 2)
+		)
+		case Phase.MID => Seq(
+			(new PawsMajorityStrategy(), 6),
+			(new KingEscapeStrategy(gameContext), 2),
+			(new HotspotStrategy(gameContext), 1),
+			(new KingKillingStrategy, 1)
+		)
+		case Phase.END => Seq(
+			(new KingKillingStrategy, 8),
+			(new PawsMajorityStrategy(), 2)
+		)
+	}
 
 	def createHeuristicFunction(gameContext: GameContext, player: Player, phase: Phase): HeuristicFunction =
 		new HeuristicFunctionImpl(player match{
-			case Player.WHITE => whiteStrategies(gameContext)
-			case Player.BLACK => blackStrategies(gameContext)
+			case Player.WHITE => whiteStrategies(gameContext, phase)
+			case Player.BLACK => blackStrategies(gameContext, phase)
 			case _ => Seq()
 		})
 }
