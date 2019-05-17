25883f92b7cdd912a84841372d7d62e491102029
Matteo <matteo.pelle.pellegrino@gmail.com>
2019-05-17 17:03:34 +0200

    Adjust padding time

--- src/main/scala/ai/tablut/Config.scala
+++ src/main/scala/ai/tablut/Config.scala
@@ -9,7 +9,7 @@ case class Config (
   whitePort: Int = 5800,
   blackPort: Int = 5801,
   maxTurnTime: Int = 60,
-  paddingTime: Int = 4,
+  paddingTime: Int = 2,
   debug: Boolean = false,
   midPhaseTurn: Int = 3,
   endPhaseTurn: Int = 6
